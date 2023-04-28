de5073d9a07c8dee3b251ad60b51b863642f79bc
JUN <chltmdwns96@naver.com>
2023-04-28 13:35:11 +0900

    test: 이동 횟수에 대한 예외 처리 테스트 구현

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -34,6 +34,16 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 이동_횟수에_대한_예외_처리() {
+        assertSimpleTest(
+                () -> {
+                    runException("pobi,java","a");
+                    assertThat(output()).contains(ERROR_MESSAGE);
+                }
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
