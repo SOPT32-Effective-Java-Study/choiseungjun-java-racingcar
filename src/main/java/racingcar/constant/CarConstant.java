cc6f968877ccdb2e0b4986aba416e2cbb0b0a3ba
PgmJun <84304802+PgmJun@users.noreply.github.com>
2023-05-05 11:16:04 +0900

    refactor: 매직 리터럴 제거

--- src/main/java/racingcar/constant/CarConstant.java
+++ src/main/java/racingcar/constant/CarConstant.java
@@ -11,6 +11,8 @@ public class CarConstant {
     public static final String RACING_WINNER_SEPARATOR = ", ";
     public static final int MOVE = 1;
     public static final int STAY = 0;
+    public static final String EMPTY_SPACE = " ";
+    public static final String NOT_EMPTY_SPACE = "";
 
 
 
